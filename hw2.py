b33745ede8433041a8911b10d108d5946b083564
Nastiona <A.N.Dontsova@yandex.ru>
2022-12-01 19:04:22 +0300

    2

---
+++ hw2.py
@@ -0,0 +1,38 @@
+# Напишите программу, которая принимает на вход вещественное число
+# и показывает сумму его цифр.
+
+# str_n = input()
+# sum = 0
+# for el in str_n:
+#     if el != '.':
+#         sum += int(el)
+# print(sum)
+
+# Напишите программу, которая принимает на вход число N
+# и выдает набор произведений чисел от 1 до N.
+
+# n = int(input())
+# some_list = []
+# fact = 1
+# for i in range(1, n + 1):
+#    fact *= i
+#    some_list.append(fact)
+# print(some_list)
+
+# Задайте список из n чисел последовательности (1 + 1 / n) ** n
+# и выведите на экран их сумму.
+
+# n = int(input())
+# sum = 0
+# for i in range(1, n + 1):
+#     sum += ((1 + 1 / i) ** i)
+# print(round(sum, 2))
+
+
+
+# Реализуйте алгоритм перемешивания списка.
+
+# import random
+# list = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+# random.shuffle(list)
+# print(list)
